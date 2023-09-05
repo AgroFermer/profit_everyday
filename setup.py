ac42633954da0bb278d8fc18e093bf9b6f36d2c7
Lemishko Dmitry <81725793+AgroFermer@users.noreply.github.com>
2023-09-05 13:05:42 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,9 @@ BOT_TOKEN = '5782607808:AAG6LpY2BUj25r1UeeWcBGNP8q7wOmZFVLQ'
 CHANNEL_ID =  -1001712236318
 ADMIN_ID = 1889004772
 async def approve_request (chat_join: ChatJoinRequest, bot: Bot):
-   msg= f"Дорогой друг, поздравляем тебя с приобретением подписки на закрытое сообщество PROFIT.\n\nВы можете войти в канал: <a href='https://t.me/+YigHdJ6h1ck3YmQy'>https://t.me/globalprofit</a>"
+   msg= f""Дорогой друг, поздравляем тебя с приобретением подписки на закрытое сообщество PROFIT.\n\n"
+        "Вы можете войти в канал: "
+        "<a href='https://t.me/+YigHdJ6h1ck3YmQy'>https://t.me/globalprofit</a>""
    button = InlineKeyboardButton(text='ВСТУПИТЬ', url='https://t.me/+YigHdJ6h1ck3YmQy', disable_web_page_preview=True)   
    markup = InlineKeyboardMarkup(inline_keyboard=[[button]])
 
